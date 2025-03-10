8144a35ccd7767d9051e05d4dfb52d7be01f9bb9
NoorAtallah <nooratallah1234@gmail.com>
2025-03-10 22:00:20 +0300

    new edits

--- petify/src/componants/api/axios.js
+++ petify/src/componants/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an Axios instance with the base URL of your backend
 const instance = axios.create({
-  baseURL: 'https://petifyback-2.onrender.com'  // Replace with your backend URL
+  baseURL: 'https://petifyback-2.onrender.com/api'  // Replace with your backend URL
 });
 
 export default instance;
